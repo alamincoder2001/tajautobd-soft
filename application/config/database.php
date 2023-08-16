f0aef409606e5fb2072958783c3952830d533bfb
alamincoder2001 <114378678+alamincoder2001@users.noreply.github.com>
2023-08-16 12:44:03 +0600

    product sales change and add all first

---
+++ application/config/database.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+
+
+    'username' => 'root',
+  	'password' => '',
+  	'database' => 'taja4tobd_possoft',
+
+	// 'username' => 'taja4tobd_admin1',
+  	// 'password' => '5c4ebct1&dS^',
+  	// 'database' => 'taja4tobd_possoft',
+
+		
+
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
